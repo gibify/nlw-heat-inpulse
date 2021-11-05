a2fd2a3bcb0872ef80c604c176aadc900dd3092a
gibify <gibifyofficial@gmail.com>
2021-11-04 21:43:51 -0300

    feat: add axios features

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -14,20 +14,36 @@ type Message = {
   }
 }
 
+let messagesQueue: Message[] = [];
+
 const socket = io('http://localhost:4000');
 
-socket.on('new_message', newMessage => {
-  console.log(newMessage);
+socket.on('new_message', (newMessage: Message) => {
+  messagesQueue.push(newMessage);
 })
 
 export function MessageList() {
   const [messages, setMessages] = useState<Message[]>([]);
 
   useEffect(() => {
+    const timer = setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setMessages(prevState => [
+          messagesQueue[0],
+          prevState[0],
+          prevState[1]
+        ].filter(Boolean))
+
+        messagesQueue.shift()
+      }
+    }, 2000)
+  }, []);
+
+  useEffect(() => {
     api.get<Message[]>('/messages/last3').then(response => {
       setMessages(response.data)
     })
-  }, [])
+  }, []);
 
   return (
     <div className={styles.messageListWrapper}>
